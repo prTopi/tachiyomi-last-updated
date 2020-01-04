80199aa7b65b04a9f0759307586396c690c6d896
Topi Harjunpää <topi@harjunpaa.fi>
2020-01-04 19:46:56 +0200

    Code formatting changes
    Single quotes to double quotes
    Shortened variable names to fit more things in to one line
    Point shebang to /usr/bin/env instead of /bin/env

--- tachiyomi-updated.py
+++ tachiyomi-updated.py
@@ -1,25 +1,21 @@
-#!/bin/env python3
+#!/usr/bin/env python
 from argparse import ArgumentParser
 from sqlite3 import connect
 from datetime import date, timedelta
 
-parser = ArgumentParser(description='Reads Tachiyomi database and lists '
-                        'all series which haven\'t been updated in '
-                        'specified time.')
-parser.add_argument('-y', '--years', type=int, help='Number of years.',
-                    default=0)
-parser.add_argument('-m', '--months', type=int, help='Number of months.',
-                    default=0)
-parser.add_argument('-d', '--days', type=int, help='Number of days.',
-                    default=0)
-args = parser.parse_args()
-with connect('tachiyomi.db') as db:
+ap = ArgumentParser(description="Reads Tachiyomi database and lists all "
+                    "series which haven't been updated in specified time.")
+ap.add_argument("-y", "--years", type=int, default=0, help="Number of years")
+ap.add_argument("-m", "--months", type=int, default=0, help="Number of months")
+ap.add_argument("-d", "--days", type=int, default=0, help="Number of days")
+args = ap.parse_args()
+with connect("tachiyomi.db") as db:
     c = db.cursor()
     mangas = dict(c.execute("SELECT _id, title FROM mangas WHERE favorite=1")
                   .fetchall())
-    print(f'You have a total of {len(mangas)} mango!\n')
-    chapters = c.execute('SELECT manga_id, date_upload FROM chapters WHERE '
-                         f'manga_id IN ({",".join("?" for _ in mangas)})',
+    print(f"You have a total of {len(mangas)} manga!\n")
+    chapters = c.execute("SELECT manga_id, date_upload FROM chapters WHERE "
+                         f"manga_id IN ({','.join('?' for _ in mangas)})",
                          list(mangas))
     chapterTimes = {x: 0 for x in mangas}
     for line in chapters:
@@ -33,4 +29,4 @@ with connect('tachiyomi.db') as db:
         if len(value) > 3:
             time = date.fromtimestamp(int(value[:-3]))
             if time < lastDate:
-                print(f'> {mangas[x]}')
+                print(f"> {mangas[x]}")
