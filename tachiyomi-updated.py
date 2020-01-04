2b5ecd6f16aa7009b8dabca51439a04e13437450
Topi Harjunpää <topi@harjunpaa.fi>
2020-01-04 22:48:50 +0200

    Formatting fixes
    Raise error if tachiyomi.db is not found
    Fixed bad database connection formatting
    Added spacing between code blocks

--- tachiyomi-updated.py
+++ tachiyomi-updated.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from os import listdir
 from argparse import ArgumentParser
 from sqlite3 import connect
 from datetime import date, timedelta
@@ -9,24 +10,32 @@ ap.add_argument("-y", "--years", type=int, default=0, help="Number of years")
 ap.add_argument("-m", "--months", type=int, default=0, help="Number of months")
 ap.add_argument("-d", "--days", type=int, default=0, help="Number of days")
 args = ap.parse_args()
-with connect("tachiyomi.db") as db:
-    c = db.cursor()
-    manga = dict(c.execute("SELECT _id, title FROM mangas WHERE favorite=1")
-                  .fetchall())
-    print("You have a total of {0} manga!\n".format(len(manga)))
-    chaps = c.execute("SELECT manga_id, date_upload FROM chapters WHERE "
-                      "manga_id IN ({0})".format(','.join('?' for _ in manga)),
-                      list(manga))
-    chapterTimes = {x: 0 for x in manga}
-    for line in chaps:
-        mangaId, value = line
-        if chapterTimes[mangaId] < value:
-            chapterTimes[mangaId] = value
-    days = int(args.years) * 365 + int(args.months) * 30.417 + int(args.days)
-    lastDate = date.today() - timedelta(days=days)
-    for x in chapterTimes:
-        value = str(chapterTimes[x])
-        if len(value) > 3:
-            time = date.fromtimestamp(int(value[:-3]))
-            if time < lastDate:
-                print(">", manga[x])
+
+if "tachiyomi.db" not in listdir("."):
+    raise FileNotFoundError("database file (tachiyomi.db) not found")
+
+db = connect("tachiyomi.db")
+c = db.cursor()
+
+manga = dict(c.execute("SELECT _id, title FROM mangas WHERE favorite=1")
+             .fetchall())
+print("You have a total of {0} manga!\n".format(len(manga)))
+chaps = c.execute("SELECT manga_id, date_upload FROM chapters WHERE "
+                  "manga_id IN ({0})".format(','.join('?' for _ in manga)),
+                  list(manga))
+db.close()
+
+chapterTimes = {x: 0 for x in manga}
+for line in chaps:
+    mangaId, value = line
+    if chapterTimes[mangaId] < value:
+        chapterTimes[mangaId] = value
+
+days = int(args.years) * 365 + int(args.months) * 30.417 + int(args.days)
+lastDate = date.today() - timedelta(days=days)
+for x in chapterTimes:
+    value = str(chapterTimes[x])
+    if len(value) > 3:
+        time = date.fromtimestamp(int(value[:-3]))
+        if time < lastDate:
+            print(">", manga[x])
